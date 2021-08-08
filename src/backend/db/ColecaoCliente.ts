9d4aca46feb2c11b810d351b27de41153c9fb26d
AnjosHenrique <79482171+AnjosHenrique@users.noreply.github.com>
2021-08-08 19:35:22 -0300

    Funcionando

--- src/backend/db/ColecaoCliente.ts
+++ src/backend/db/ColecaoCliente.ts
@@ -4,9 +4,9 @@ import ClienteRepositorio from "../../core/ClienteRepositorio";
 
 export default class ColecaoCliente implements ClienteRepositorio {
 
-    conversor = {
+    #conversor = {
         toFirestore(cliente: Cliente) {
-            return{
+            return {
                 nome: cliente.nome,
                 idade: cliente.idade,
             }
@@ -17,19 +17,29 @@ export default class ColecaoCliente implements ClienteRepositorio {
         }
     }
     
-    async salvar(cliente: Cliente): Promise<Cliente>{
-        return null
-    }
-    
-    async excluir(cliente: Cliente): Promise<void>{
-        return null
+    async salvar(cliente: Cliente): Promise<Cliente> {
+        if(cliente?.id) {
+            await this.colecao().doc(cliente.id).set(cliente)
+            return cliente
+        } else {
+            const docRef = await this.colecao().add(cliente)
+            const doc = await docRef.get()
+            return doc.data()
+        }
     }
 
-    async ObterTodos(): Promise<Cliente[]>{
-        return null
+    async excluir(cliente: Cliente): Promise<void> {
+        return this.colecao().doc(cliente.id).delete()
+    }
+
+    async obterTodos(): Promise<Cliente[]> {
+        const query = await this.colecao().get()    
+        return query.docs.map(doc => doc.data()) ?? []
     }
 
     private colecao() {
-
+        return firebase
+            .firestore().collection('clientes')
+            .withConverter(this.#conversor)
     }
 }
